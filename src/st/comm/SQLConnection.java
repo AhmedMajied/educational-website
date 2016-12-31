36dc31067b0e48e62e5a161b6f9b50f69e11b2f5
Ahmed <aabdelmeged70@yahoo.com>
2016-12-31 11:54:30 +0200

    initial

---
+++ src/st/comm/SQLConnection.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package st.comm;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Andrew
+ */
+public class SQLConnection {
+
+    public static Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+        String dbURL = "jdbc:sqlserver://AHMED\\AHMED_HUSSEIN;databaseName=ST-Comm;integratedSecurity=true;";
+        Connection conn = DriverManager.getConnection(dbURL);
+        return conn;
+    }
+}
